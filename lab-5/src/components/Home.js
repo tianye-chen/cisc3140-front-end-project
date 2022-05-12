fa56dc7eccddb7f2ffdc5100959846daf0327b36
Tianye Chen <78762926+tianye-chen@users.noreply.github.com>
2022-05-11 21:50:25 -0400

    push

--- lab-5/src/components/Home.js
+++ lab-5/src/components/Home.js
@@ -3,7 +3,19 @@ import React from "react";
 export default function Home(){
     return (
         <div>
-            <p>Homepage</p>
+            <h1>CISC 3140 LAB 5</h1>
+            <h2>
+                Connects to backend:
+                <h5>
+                    <a href="https://github.com/tianye-chen/cisc3140-backend-project">
+                        https://github.com/tianye-chen/cisc3140-backend-project
+                    </a>
+                </h5>
+            </h2>
+            <p>This is a simple car-judge management system</p>
+            <h3>Collaborators: </h3>
+            <p>Tianye Chen</p>
+            <p>Bohui Nong</p>
         </div>
     );
 }
